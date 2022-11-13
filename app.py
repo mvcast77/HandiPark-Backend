5472f48628f3db914190488a800cc0cc7b1cece0
mvcast77 <mvcastillo@scu.edu>
2022-11-12 22:42:01 -0800

    Hard Coding Sensitive Information

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ load_dotenv()
 
 app = Flask(__name__)
 
-APP_ID = os.getenv('APP_ID')
-APP_KEY = os.getenv('APP_KEY')
-HASH_TOKEN = os.getenv('HASH_TOKEN')
-EXPIRATION_DATE = os.getenv('EXPIRATION_DATE')
-BASE_URL = os.getenv('BASE_URL')
+APP_ID = '1c3u7ib9k2' # os.getenv('APP_ID')
+APP_KEY = 'uQLYgW5zay6z53r4BV4bk3fAS2S7dRlt7WGsFcHU' # os.getenv('APP_KEY')
+HASH_TOKEN = 'MWMzdTdpYjlrMnx1UUxZZ1c1emF5Nno1M3I0QlY0YmszZkFTMlM3ZFJsdDdXR3NGY0hV' # os.getenv('HASH_TOKEN')
+EXPIRATION_DATE = '2022-11-27T20:05:09.645Z' # os.getenv('EXPIRATION_DATE')
+BASE_URL = 'https://api.iq.inrix.com/' # os.getenv('BASE_URL')
 
 @app.route('/viola', methods = ['GET'])
 def viola():
